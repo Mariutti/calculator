2bcfa845673d6c829ad4fede90ee9ef8211eff00
Mariutti <pedromariutti@gmail.com>
2023-01-15 12:43:11 -0300

    Give some buttons functions

--- script.js
+++ script.js
@@ -1,66 +1,105 @@
 function add(nOne, nTwo){
     return nOne + nTwo;
-  }
-  
-  function subtract(nOne, nTwo){
+}
+
+function subtract(nOne, nTwo){
     return nOne - nTwo;
-  }
-  
-  function multiply(nOne, nTwo){
+}
+
+function multiply(nOne, nTwo){
     return nOne * nTwo;
-  }
-  
-  function divide(nOne, nTwo){
+}
+
+function divide(nOne, nTwo){
     return nOne / nTwo;
-  }
-  
-  function operate(operator, nOne, nTwo){
+}
+
+function operate(operator, nOne, nTwo){
     switch(operator){
-      case add:
+        case add:
         return add(nOne, nTwo);
-      case subtract:
+        case subtract:
         return subtract(nOne, nTwo)
-      case multiply:
+        case multiply:
         return multiply(nOne, nTwo)
-      case divide:
+        case divide:
         return divide(nOne, nTwo)
     }
-  }
+}
 
-  const mainNumber = document.querySelector('.main-number')
+const mainNumber = document.querySelector('.main-number')
 
-  const supNumber = document.querySelector('.superiorNumber')
+const supNumber = document.querySelector('.superiorNumber')
 
-  const operatorScreen = document.querySelector('.operatorScr')
+const operatorScreen = document.querySelector('.operatorScr')
 
-  const btnNumberList = Array.from(document.querySelectorAll('.numberBtn'))
+const btnNumberList = Array.from(document.querySelectorAll('.numberBtn'))
 
-  btnNumberList.forEach(btn => {
+const symbolShower = document.querySelector('.operatorScr');
+
+btnNumberList.forEach(btn => {
     btn.addEventListener('click', btnNumberFunction)
-  });
+});
 
-  function btnNumberFunction(e){
-    let number = parseFloat(e.target.textContent)
+const btnSpecialFuncList = Array.from(document.querySelectorAll('.specialBtn'))
+
+btnSpecialFuncList.forEach(btn => {
+    btn.addEventListener('click', btnSpecialFunc)
+});
+
+
+function btnNumberFunction(e){
+    let number = e.target.textContent
     numbScreen = mainNumber.textContent
-
+    
     if(mainNumber.textContent == 0){
         mainNumber.textContent = number
     }else{
         if(mainNumber.textContent.length < 11){
-        mainNumber.textContent += number
+            mainNumber.textContent += number
         } else {
             if(mainNumber.textContent.length === 11){
-            const list = Array.from(mainNumber.textContent);
-            list.shift();
-            mainNumber.textContent = list.join('')
+                const list = Array.from(mainNumber.textContent);
+                list.shift();
+                mainNumber.textContent = list.join('')
             }
             mainNumber.textContent += number
         }
-
     }
-
-    
-    console.log(number)
 }
 
-  
+const btnOperatorList = Array.from(document.querySelectorAll('.operatorBtn'))
+
+btnOperatorList.forEach(btn => {
+    btn.addEventListener('click', btnOperatorFunc)
+});
+let numSup;
+let numInf;
+
+function btnOperatorFunc(e){
+    btnSymbol = e.target.textContent    
+    symbolShower.textContent = btnSymbol;
+    numSup = parseFloat(mainNumber.textContent)
+    
+}
+
+function btnSpecialFunc(e){
+    let btnSpec = e.target.textContent
+    console.log(btnSpec)
+    switch (btnSpec.textContent) {
+        case 'CE':
+            console.log('CE')
+            break;
+        case 'C':
+            console.log('C')
+            break;
+        case '⌫':
+            console.log('⌫')
+            break;
+        case '+/-':
+            console.log('+/-')
+            break;
+        default:
+            break;
+    }
+}
